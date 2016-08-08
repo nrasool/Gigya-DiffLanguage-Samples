5de2da3604072b30e486f3838415a82b3392c9eb
Nadeem Rasool <nrasool.developer@gmail.com>
2016-08-08 13:20:26 +0100

    Add files via upload
    Updated source file with help in comments

--- auditsearch-GigyaPHP-Sample.php
+++ auditsearch-GigyaPHP-Sample.php
@@ -7,13 +7,15 @@ include_once "GSSDK.php";
 // Define the API-Key and Secret key (the keys can be obtained from your site setup page on Gigya's website).
 $apiKey = "xxxxxx";
 $userkey = "xxxxxx";
-$secretKey = "xxxxxx";;
+$secretKey = "xxxxxx"; // This must be the secretkey for your userkey, NOT PARTNERID secret key
 $method = "audit.search";
 
 // Request
 $request = new GSRequest($apiKey,$secretKey,$method,null, true);
 $request->setParam("userKey",$userkey); 
 $request->setParam("query", "SELECT * FROM auditLog where endpoint like 'accounts.%'");  // Running SQL query
+
+// Set your domain name to which the API key is residing
 $request->setAPIDomain("us1.gigya.com"); 
 
 //Sending the request
